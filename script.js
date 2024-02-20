90e29f378ab762cce5f061382f4bf97c59bb42c1
Vitória Cardoso <128175177+Vitoriarochacardoso@users.noreply.github.com>
2024-02-19 22:36:47 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let gostoUsuario;
+let respostaUsuario;
+
+gostoUsuario = prompt("Quer seguir para a área de Front-End ou Back-End? Escolha 1..");
+
+if (gostoUsuario == 1) {alert("Gostaria de aprender React ou aprender Vue?");
+}
+
+else{alert("Gostaria de aprender C# ou aprender Java?"); 
+}
+  
